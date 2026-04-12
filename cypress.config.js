0fba4c1afe0ab52b0fe75028c25bf0af52cc10b7
MarcosAFragoso <marcosaafragoso@gmail.com>
2026-04-12 18:52:28 -0300

    cypress:teste4

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    baseUrl: 'http://localhost:3000', // ajuste para sua porta local
     setupNodeEvents(on, config) {
-      config.env.USER_EMAIL = "teste@hyperpneus.com";
+      // implement node event listeners here
+    },
+    env: {
+      clienteEmail: "teste@hyperpneus.com",
+      clienteSenha: "123" // adicione a senha se o teste pedir
     },
   },
 });
